2a3c4efd25f6f5ce61994ef76855442541356536
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-22 17:49:48 +0200

    création de script ServeurHTTP.py

---
+++ serveur_http.py
@@ -0,0 +1,7 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+from services import Francais, Anglais, HorlogeSysteme, AnalyseurTexte
+
+
+class RequetesHandler(BaseHTTPRequestHandler):
+   pass
