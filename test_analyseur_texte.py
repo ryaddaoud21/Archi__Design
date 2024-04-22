7e3900e00445ce11fc0f38f8e1520587ed7adda0
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-22 17:18:34 +0200

    initialisation des tests unitaires

---
+++ test_analyseur_texte.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import MagicMock
+from services import Francais, HorlogeSysteme, AnalyseurTexte
+
+class TestAnalyseurTexte(unittest.TestCase):
+    def setUp(self):
+        self.langue = Francais()
+        self.horloge = HorlogeSysteme()
+
+    def test_analyser_chaine_palindrome(self):
+        texte_palindrome = "radar"
+        analyseur = AnalyseurTexte(self.langue, self.horloge)
+        resultat = analyseur.analyser_chaine(texte_palindrome)
+        self.assertEqual(resultat, f"{texte_palindrome}  {self.langue.feliciter()}")
+
+    def test_analyser_chaine_non_palindrome(self):
+        texte_non_palindrome = "hello"
+        analyseur = AnalyseurTexte(self.langue, self.horloge)
+        resultat = analyseur.analyser_chaine(texte_non_palindrome)
+        self.assertEqual(resultat, texte_non_palindrome[::-1])
+
+if __name__ == "__main__":
+    unittest.main()
