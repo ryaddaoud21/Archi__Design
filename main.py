dd30434a8b85998d09e3a8268c7e617184b36848
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-22 15:19:17 +0200

    Freestyle - dire au revoir si exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def saluer(langue='fr'):
     heure = datetime.datetime.now().hour
     if langue == 'fr':
@@ -15,9 +16,26 @@ def saluer(langue='fr'):
     else:
         return "Hello"
 
+
 def est_palindrome(texte):
     texte_nettoye = ''.join(char.lower() for char in texte if char.isalnum())
     return texte_nettoye == texte_nettoye[::-1]
+
+
+def dire_au_revoir(langue='fr'):
+    heure = datetime.datetime.now().hour
+    if langue == 'fr':
+        if 6 <= heure < 18:
+            return "Au revoir, passez une bonne journée !"
+        else:
+            return "Bonne nuit, à bientôt !"
+    elif langue == 'en':
+        if 6 <= heure < 18:
+            return "Goodbye, have a nice day!"
+        else:
+            return "Good night, see you soon!"
+    else:
+        return "Goodbye!"
 
 
 def main():
@@ -26,6 +44,10 @@ def main():
 
     while True:
         texte = input("Écrivez quelque chose: ")
+        if texte.lower() == 'quit':
+            print(dire_au_revoir(langue))
+            break
+
         if est_palindrome(texte):
             if langue == 'fr':
                 print("Bien dit !")
