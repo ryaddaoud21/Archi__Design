d8bc595acd5825d5f54cc0169563397def8f9374
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-22 16:39:16 +0200

    l'ajout de l'horloge dans la constructeur de AnalyseurTexte

--- interface.py
+++ interface.py
@@ -39,22 +39,6 @@ class Anglais(Langue):
     def acquitter(self) -> str:
         return "Goodbye, have a nice day!"
 
-class InterfaceAnalyseur(ABC):
-    @abstractmethod
-    def analyser_chaine(self, chaine: str) -> str:
-        pass
-
-class AnalyseurTexte(InterfaceAnalyseur):
-    def __init__(self, langue: Langue):
-        self.langue = langue
-
-    def analyser_chaine(self, chaine: str) -> str:
-        chaine_inverse = chaine[::-1]
-        if chaine.lower() == chaine_inverse.lower():
-            return f"{chaine} {self.langue.feliciter()}"
-        else:
-            return chaine_inverse
-
 class Horloge(ABC):
     @abstractmethod
     def heure_actuelle(self) -> int:
@@ -63,6 +47,26 @@ class Horloge(ABC):
 class HorlogeSysteme(Horloge):
     def heure_actuelle(self) -> int:
         return datetime.datetime.now().hour
+
+class InterfaceAnalyseur(ABC):
+    @abstractmethod
+    def analyser_chaine(self, chaine: str) -> str:
+        pass
+
+class AnalyseurTexte(InterfaceAnalyseur):
+    def __init__(self, langue: Langue, horloge: Horloge):
+        self.langue = langue
+        self.horloge = horloge
+
+
+    def analyser_chaine(self, chaine: str) -> str:
+        chaine_inverse = chaine[::-1]
+        if chaine.lower() == chaine_inverse.lower():
+            return f"{chaine} {self.langue.feliciter()}"
+        else:
+            return chaine_inverse
+
+
 
 def main():
     langue = input("Choisissez votre langue (fr, en): ")
