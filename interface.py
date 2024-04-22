50aa70fdc657f116a60eb4c416b1c193e40efdaf
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-22 17:04:34 +0200

    gestion des erreurs  - choix de la langue .

--- interface.py
+++ interface.py
@@ -86,21 +86,21 @@ class AnalyseurTexte(InterfaceAnalyseur):
 
 
 def main():
-    langue = input("Choisissez votre langue (fr, en): ")
-    if langue == 'fr':
-        langue_instance = Francais()
-    elif langue == 'en':
-        langue_instance = Anglais()
-    else:
-        print("Langue non supportée")
-        return
+    while True:
+        langue = input("Choisissez votre langue (fr, en): ")
+        if langue == 'fr':
+            langue_instance = Francais()
+            break
+        elif langue == 'en':
+            langue_instance = Anglais()
+            break
+        else:
+            print("Langue non supportée. Veuillez choisir entre 'fr' pour le français et 'en' pour l'anglais.")
 
     horloge = HorlogeSysteme()
     analyser = AnalyseurTexte(langue_instance, horloge)
 
     print(langue_instance.saluer(horloge.heure_actuelle()))
-
-
 
     while True:
         texte = input("Écrivez quelque chose: ")
