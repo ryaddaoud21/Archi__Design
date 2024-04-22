2b20b65ac264c52881c7ca90029bb68bfc0d128c
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-22 15:18:19 +0200

    Freestyle - tester si l'input est est_palindrome

--- main.py
+++ main.py
@@ -15,10 +15,25 @@ def saluer(langue='fr'):
     else:
         return "Hello"
 
+def est_palindrome(texte):
+    texte_nettoye = ''.join(char.lower() for char in texte if char.isalnum())
+    return texte_nettoye == texte_nettoye[::-1]
+
+
 def main():
     langue = input("Choisissez votre langue (fr, en): ")
     print(saluer(langue))
 
+    while True:
+        texte = input("Écrivez quelque chose: ")
+        if est_palindrome(texte):
+            if langue == 'fr':
+                print("Bien dit !")
+            elif langue == 'en':
+                print("Well said!")
+            else:
+                print("Well done!")
+        print(f"Mirroir: {texte[::-1]}")
 
 
 if __name__ == "__main__":
