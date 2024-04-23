fd1fcd565e134cf95b45d970c43757fa88abe53d
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-23 14:50:16 +0200

    definition des methodes  GET et POST en HTTP

--- Source/Application/HTTP/serveur_http.py
+++ Source/Application/HTTP/serveur_http.py
@@ -1,34 +1,47 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 from Source.Domain.InterfaceAnalyseur import AnalyseurTexte
-from Source.Domain.Langue.LanguesSupportees import Anglais,Francais
+from Source.Domain.Langue.LanguesSupportees import Francais, Anglais
 from Source.Infrastructure.HorlogeSysteme import HorlogeSysteme
-
+import json
 
 
 class RequetesHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        langue = self.headers.get('Accept-Language', 'fr')
+        horloge = HorlogeSysteme()
+        salutation = self._choisir_langue(langue).saluer(horloge.heure_actuelle())
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"<html><body><h1>GET request received!</h1></body></html>")
+        self.wfile.write(salutation.encode('utf-8'))
 
     def do_POST(self):
         contenu_longueur = int(self.headers['Content-Length'])
         contenu = self.rfile.read(contenu_longueur).decode('utf-8')
-        langue = self.headers.get('Accept-Language', 'fr')
+        data = json.loads(contenu)
+
+        texte = data.get('texte', '')
+        langue = data.get('langue', 'fr')
+
         horloge = HorlogeSysteme()
         analyser = AnalyseurTexte(self._choisir_langue(langue), horloge)
-        resultat = analyser.analyser_chaine(contenu)
+        resultat = analyser.analyser_chaine(texte)
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(resultat.encode('utf-8'))
+
+        response_data = {
+            'texte_inverse': resultat,
+        }
+        self.wfile.write(json.dumps(response_data).encode('utf-8'))
 
     def _choisir_langue(self, langue):
-        if langue.startswith('fr'):
+        if langue == 'fr':
             return Francais()
-        elif langue.startswith('en'):
+        elif langue == 'en':
             return Anglais()
         else:
             return Francais()  # Utilisation de Français par défaut
@@ -36,7 +49,7 @@ class RequetesHandler(BaseHTTPRequestHandler):
 
 def main():
     port = 8000
-    serveur = HTTPServer(('', port), RequetesHandler)
+    serveur = HTTPServer(('localhost', port), RequetesHandler)
     print(f'Serveur démarré sur le port {port}')
     serveur.serve_forever()
 
