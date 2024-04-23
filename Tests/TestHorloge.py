22101f46db02461d4baf4f5c2add83da90df593d
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-23 10:09:38 +0200

    implémentation de test pour la classe d'interface TestHorloge

---
+++ Tests/TestHorloge.py
@@ -0,0 +1,10 @@
+import unittest
+from Infrastructure.HorlogeSysteme import HorlogeSysteme
+
+class TestHorloge(unittest.TestCase):
+    def test_time(self):
+        horloge = HorlogeSysteme()
+        self.assertIsNotNone(horloge.heure_actuelle())
+
+if __name__ == '__main__':
+    unittest.main()
