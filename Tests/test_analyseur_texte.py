b19c7436fae626de8fcb9632d896640da227d408
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-23 10:08:55 +0200

    implémentation de test pour la classe d'interface AnalyseurTexte

--- Tests/test_analyseur_texte.py
+++ Tests/test_analyseur_texte.py
@@ -1,22 +1,15 @@
 import unittest
-from Domain.services import Francais, AnalyseurTexte
+from Domain.services import  AnalyseurTexte, Francais
 from Infrastructure.HorlogeSysteme import HorlogeSysteme
+
+
 class TestAnalyseurTexte(unittest.TestCase):
-    def setUp(self):
-        self.langue = Francais()
-        self.horloge = HorlogeSysteme()
+    def test_palindrome_detection(self):
+        langue = Francais()
+        horloge = HorlogeSysteme()
+        analyseur = AnalyseurTexte(langue, horloge)
+        self.assertTrue(analyseur.est_palindrome("radar"))
+        self.assertFalse(analyseur.est_palindrome("hello"))
 
-    def test_analyser_chaine_palindrome(self):
-        texte_palindrome = "radar"
-        analyseur = AnalyseurTexte(self.langue, self.horloge)
-        resultat = analyseur.analyser_chaine(texte_palindrome)
-        self.assertEqual(resultat, f"{texte_palindrome}  {self.langue.feliciter()}")
-
-    def test_analyser_chaine_non_palindrome(self):
-        texte_non_palindrome = "hello"
-        analyseur = AnalyseurTexte(self.langue, self.horloge)
-        resultat = analyseur.analyser_chaine(texte_non_palindrome)
-        self.assertEqual(resultat, texte_non_palindrome[::-1])
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
