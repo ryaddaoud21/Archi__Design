e4eb8afc0610e803d414099b9e17be1c80e430e5
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-24 16:42:55 +0200

    [DESIGN]: Amélioration de la gestion des erreurs dans la fonction main()

--- Source/Application/HTTP/Server.py
+++ Source/Application/HTTP/Server.py
@@ -2,12 +2,21 @@ from http.server import HTTPServer
 from Source.Application.HTTP.RequetesHandler import RequetesHandler
 from Source.Application.HTTP.Configuration import SERVER_PORT,SERVER_HOST
 
+
 def main():
     port = SERVER_PORT
     server_address = (SERVER_HOST, port)
-    server = HTTPServer(server_address, RequetesHandler)
-    print(f'Serveur démarré sur le port {port}')
-    server.serve_forever()
+
+    try:
+        server = HTTPServer(server_address, RequetesHandler)
+        print(f'Serveur démarré sur le port {port}')
+        server.serve_forever()
+    except Exception as e:
+        print(f"Erreur lors de l'exécution du serveur: {e}")
+    finally:
+        print("Arrêt du serveur.")
+        server.server_close()
+
 
 if __name__ == "__main__":
     main()
