12eb9bec401925784a5e26e17b0b404280b697b4
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-24 10:25:50 +0200

    [DESIGN] : Amélioration de la gestion des langues dans le choix utilisateur et la création, modularisation de la logique de saisie, de salutation et d'analyse de texte ( en ajoutant analyser_texte() , afficher_salutation() , choisir_langue() ) pour separer les responsabilites , et ajout de commentaires pour une meilleure lisibilité du code.

--- Source/Application/Main/main_interface.py
+++ Source/Application/Main/main_interface.py
@@ -1,23 +1,35 @@
-from Source.Domain.InterfaceAnalyseur import AnalyseurTexte
 from Source.Domain.Langue.FabriqueLangue import FabriqueLangue
 from Source.Infrastructure.HorlogeSysteme import HorlogeSysteme
+from Source.Domain.InterfaceAnalyseur import AnalyseurTexte
 
 
-def main():
+def choisir_langue():
     while True:
         langue_code = input("Choisissez votre langue (fr, en): ")
         try:
             langue = FabriqueLangue.creer_langue(langue_code)
-            break
+            return langue
         except ValueError as e:
-            print(e)
+            print(f"Erreur: {e}")
             print("Langue non supportée. Veuillez choisir entre 'fr' pour le français et 'en' pour l'anglais.")
 
 
+def afficher_salutation(langue, horloge):
+    print(langue.saluer(horloge.heure_actuelle()))
+
+
+def analyser_texte(analyseur, texte):
+    resultat = analyseur.analyser_chaine(texte)
+    print(resultat)
+    print(f"Mirroir: {texte[::-1]}")
+
+
+def main():
+    langue = choisir_langue()
     horloge = HorlogeSysteme()
     analyser = AnalyseurTexte(langue, horloge)
 
-    print(langue.saluer(horloge.heure_actuelle()))
+    afficher_salutation(langue, horloge)
 
     while True:
         texte = input("Écrivez quelque chose: ")
@@ -25,8 +37,7 @@ def main():
             print(langue.acquitter(horloge.heure_actuelle()))
             break
 
-        print(analyser.analyser_chaine(texte))
-        print(f"Mirroir: {texte[::-1]}")
+        analyser_texte(analyser, texte)
 
 
 if __name__ == "__main__":
