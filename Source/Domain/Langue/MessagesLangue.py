b6d1cd651654da06cd8437a9af9b44e3c8f8a038
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-04-24 10:18:00 +0200

    [DESIGN] :séparation des messages de retour de chaque langue dans un autre script pour mieux organiser le code et de séparer les responsabilités liées aux langues et aux messages spécifiques.

---
+++ Source/Domain/Langue/MessagesLangue.py
@@ -0,0 +1,31 @@
+MESSAGES_FR = {
+    'salutations': {
+        'matin': "Bonjour",
+        'apres_midi': "Bon après-midi",
+        'soir': "Bonsoir",
+        'nuit': "Bonne nuit"
+    },
+    'acquittements': {
+        'journee': "Bonne journée",
+        'apres_midi': "Bonne après-midi",
+        'soiree': "Bonne soirée",
+        'nuit': "Bonne nuit"
+    },
+    'felicitation': "Bien dit !"
+}
+
+MESSAGES_EN = {
+    'salutations': {
+        'matin': "Good Morning",
+        'apres_midi': "Good Afternoon",
+        'soir': "Good Evening",
+        'nuit': "Good Night"
+    },
+    'acquittements': {
+        'journee': "Good Day",
+        'apres_midi': "Good Afternoon",
+        'soiree': "Good Evening",
+        'nuit': "Good Night"
+    },
+    'felicitation': "Well said!"
+}
