0919ec776574020f2096b01bc8039e3548879ec6
ryaddaoud21 <m.daoud@esi-sba.dz>
2024-05-19 16:13:49 +0200

    Refactorisation : Mise à jour de la structure de la fabrique de langues

--- Source/Langue/LanguesSupportees.py
+++ Source/Langue/LanguesSupportees.py
@@ -1,30 +1,22 @@
 from Source.Domain.InterfaceLangue import Langue
 from Source.Langue.MessagesLangue import MESSAGES_FR, MESSAGES_EN
 
-
 class Francais(Langue):
     def saluer(self, heure: int) -> str:
-        # Utilisation des messages de langue française
-        return MESSAGES_FR['salutations']['matin'] if 6 <= heure < 12 else MESSAGES_FR['salutations']['soir']
+        return f"{MESSAGES_FR['salutations']['matin']}" if 6 <= heure < 12 else f"{MESSAGES_FR['salutations']['soir']}"
 
     def acquitter(self, heure: int) -> str:
-        # Utilisation des messages de langue française
-        return MESSAGES_FR['acquittements']['journee'] if 6 <= heure < 12 else MESSAGES_FR['acquittements']['soiree']
+        return f"{MESSAGES_FR['acquittements']['journee']}" if 6 <= heure < 12 else f"{MESSAGES_FR['acquittements']['soiree']}"
 
     def feliciter(self) -> str:
-        # Utilisation des messages de langue française
         return MESSAGES_FR['felicitation']
-
 
 class Anglais(Langue):
     def saluer(self, heure: int) -> str:
-        # Utilisation des messages de langue anglaise
-        return MESSAGES_EN['salutations']['matin'] if 6 <= heure < 12 else MESSAGES_EN['salutations']['soir']
+        return f"{MESSAGES_EN['salutations']['morning']}" if 6 <= heure < 12 else f"{MESSAGES_EN['salutations']['evening']}"
 
     def acquitter(self, heure: int) -> str:
-        # Utilisation des messages de langue anglaise
-        return MESSAGES_EN['acquittements']['journee'] if 6 <= heure < 12 else MESSAGES_EN['acquittements']['soiree']
+        return f"{MESSAGES_EN['acquittements']['day']}" if 6 <= heure < 12 else f"{MESSAGES_EN['acquittements']['evening']}"
 
     def feliciter(self) -> str:
-        # Utilisation des messages de langue anglaise
         return MESSAGES_EN['felicitation']
--- Source/Langue/MessagesLangue.py
+++ Source/Langue/MessagesLangue.py
@@ -1,31 +1,23 @@
 MESSAGES_FR = {
     'salutations': {
         'matin': "Bonjour",
-        'apres_midi': "Bon après-midi",
-        'soir': "Bonsoir",
-        'nuit': "Bonne nuit"
+        'soir': "Bonsoir"
     },
     'acquittements': {
         'journee': "Bonne journée",
-        'apres_midi': "Bonne après-midi",
-        'soiree': "Bonne soirée",
-        'nuit': "Bonne nuit"
+        'soiree': "Bonne soirée"
     },
     'felicitation': "Bien dit !"
 }
 
 MESSAGES_EN = {
     'salutations': {
-        'matin': "Good Morning",
-        'apres_midi': "Good Afternoon",
-        'soir': "Good Evening",
-        'nuit': "Good Night"
+        'morning': "Good Morning",
+        'evening': "Good Evening"
     },
     'acquittements': {
-        'journee': "Good Day",
-        'apres_midi': "Good Afternoon",
-        'soiree': "Good Evening",
-        'nuit': "Good Night"
+        'day': "Good Day",
+        'evening': "Good Evening"
     },
     'felicitation': "Well said!"
 }
